8ef618d91b2a4e522730adf878dcb666c84c23e8
Anderson Ferreira <andersonferreira251139@gmail.com>
2024-07-03 11:28:10 -0300

    update

--- src/components/MovieTemplate/MovieTemplate.jsx
+++ src/components/MovieTemplate/MovieTemplate.jsx
@@ -5,20 +5,15 @@ import { Link } from "react-router-dom";
 
 const MovieTemplate = ({ movie, showLink = true }) => {
   return (
-    <div className="container-movie-template">
-      <div className="container-img">
-        <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
-      </div>
+    <a href={`movie/${movie.id}`} className="link">
+      <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
+
       <h2>{movie.title}</h2>
       <p>
-        <FaStar /> {movie.vote_average.toFixed(1)}
+       {movie.vote_average.toFixed(1)}
       </p>
-      {showLink && (
-        <Link className="button-template" to={`/movie/${movie.id}`}>
-          Detalhes
-        </Link>
-      )}
-    </div>
+  
+    </a>
   );
 };
 
